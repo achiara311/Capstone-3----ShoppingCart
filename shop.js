5eaeba75c71d0e7d4d0dadb8d28d1ffd8e706197
Anthony Chiara <achiara12@gmail.com>
2019-08-12 22:40:24 -0400

    Some changes

--- shop.js
+++ shop.js
@@ -1,10 +1,4 @@
-function shopList(){
-let item = document.getElementById("todoForShopping").value
-let text = document.createTextNode(item);
-let newItem = document.createElement("li")
-newItem.appendChild(text)
-document.getElementById("theShopList").appendChild(newItem)
-}
+
 
 
 
